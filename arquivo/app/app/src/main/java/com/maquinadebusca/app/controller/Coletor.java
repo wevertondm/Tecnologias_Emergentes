fb47a2eceb962cc4866f63cd748e7e1ec721dd30
Weverton Monteiro <wevertonwdm@hotmail.com>
2019-03-26 23:09:39 -0300

    Atividade 2

--- arquivo/app/app/src/main/java/com/maquinadebusca/app/controller/Coletor.java
+++ arquivo/app/app/src/main/java/com/maquinadebusca/app/controller/Coletor.java
@@ -1,36 +1,73 @@
 
 package com.maquinadebusca.app.controller;
 
-import java.io.BufferedReader; import java.io.IOException; 
-import java.io.InputStream; import java.io.InputStreamReader; 
-import java.net.URL; import java.net.URLConnection; 
+import java.net.URL; 
+import java.util.List; 
+import java.util.LinkedList;
+import org.jsoup.Jsoup; 
+import org.jsoup.nodes.Document; 
+import org.jsoup.nodes.Element; 
+import org.jsoup.select.Elements; 
 import org.springframework.http.MediaType; 
 import org.springframework.web.bind.annotation.GetMapping; 
 import org.springframework.web.bind.annotation.RestController; 
 import org.springframework.web.bind.annotation.RequestMapping; 
+import com.maquinadebusca.app.model.Documento;
 
 @RestController 
 @RequestMapping ("/coletor") // URL: http://localhost:8080/coletor 
 public class Coletor {
     // URL: http://localhost:8080/coletor/iniciar    
-    @GetMapping (value = "/iniciar", produces = MediaType.TEXT_PLAIN_VALUE)
- public String iniciar () {        
-      StringBuilder pagina = new StringBuilder ();     
-      try {    
-          URL url = new URL
-         ("http://journals.ecs.soton.ac.uk/java/tutorial/networking/urls/readingWriting.html");
-          URLConnection url_connection = url.openConnection ();   
-          InputStream is = url_connection.getInputStream ();  
-          InputStreamReader reader = new InputStreamReader (is);   
-          BufferedReader buffer = new BufferedReader (reader);
-          String linha;      
- while ((linha = buffer.readLine ()) != null) { 
-        pagina.append (linha);    
-    }  
-  } catch (IOException e) {
-    pagina.append ("Erro: não foi possível coletar a página.");   
-  }     
-    return pagina.toString (); 
-      }  
+    @GetMapping (value = "/iniciar", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    
+      public Documento iniciar (){
+          URL url;
+          Documento d= new Documento();
+          try{
+              url=new URL
+          ("http://journals.ecs.soton.ac.uk/java/tutorial/networking/urls/readingWriting.html");
+           
+              Document doc = Jsoup.connect (url.toString()).get(); 
+              Elements links=doc.select("a[href]");
+              
+              d.setUrl(url);
+              d.setTexto(doc.html());
+              d.setVisao(doc.text());
+              
+              List<String> urls=new LinkedList();
+              for (Element link:links);
+              if ((!link.attr("abs:href").equals("")&&(link.attr("abs:href")!=null)))
+              urls.add(link.attr("abs:href"));
+              d.setUrl(urls);
+              
+             System.out.println ("\n\n\n=================================================");       
+             System.out.println (">>> URL:");       
+             System.out.println ("=================================================");
+             System.out.println (d.getUrl ()); 
+             
+             System.out.println ("\n\n\n=================================================");       
+             System.out.println (">>> Página:");       
+             System.out.println ("=================================================");       
+             System.out.println (d.getTexto ()); 
+ 
+             System.out.println ("\n\n\n=================================================");       
+             System.out.println (">>> Visão:");       
+             System.out.println ("=================================================");       
+             System.out.println (d.getVisao ()); 
+              
+             System.out.println("\n\n\n=================================================");
+                 
+             System.out.println(">>>URLs: ");
+             System.out.println("=================================================");
+             urls=d.getUrls();
+             for (String u: urls)
+                 System.out.println(u);
+             { catch (Exception e)}
+             System.out.println("Erro ao coletar a pagina");
+             e.printStarkTrace();
+          }
+      }
+      return d;
+         }  
  }   
 
---
+++ arquivo/app/app/src/main/java/com/maquinadebusca/app/model/Documento.java
@@ -0,0 +1,56 @@
+
+package com.maquinadebusca.app.model;
+
+import java.net.URL; 
+import java.util.List;
+public class Documento {
+  private URL url; 
+  private String texto; 
+  private String visao; 
+  private List<String> urls;
+
+    public Documento(URL url, String texto, String visao, List<String> urls) {
+        this.url = url;
+        this.texto = texto;
+        this.visao = visao;
+        this.urls = urls;
+    }
+
+    public Documento() {
+    }
+
+    public URL getUrl() {
+        return url;
+    }
+
+    public void setUrl(URL url) {
+        this.url = url;
+    }
+
+    public String getTexto() {
+        return texto;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+
+    public String getVisao() {
+        return visao;
+    }
+
+    public void setVisao(String visao) {
+        this.visao = visao;
+    }
+
+    public List<String> getUrls() {
+        return urls;
+    }
+
+    public void setUrls(List<String> urls) {
+        this.urls = urls;
+    }
+  
+  
+    
+}
